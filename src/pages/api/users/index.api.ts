1e68b177a84f87459f14b8a1baf22b5b080d02f0
mtguerson <mtguerson@gmail.com>
2024-08-11 18:44:15 -0400

    feat: create prisma user

---
+++ src/pages/api/users/index.api.ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { prisma } from '@/lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const { username, name } = req.body
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      name,
+    },
+  })
+
+  return res.status(201).json(user)
+}
