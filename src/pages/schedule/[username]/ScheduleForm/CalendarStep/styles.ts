4cda736aa9773553168e59a33b6ec738048324c1
mtguerson <mtguerson@gmail.com>
2024-08-14 21:30:38 -0400

    feat: add style to calendar

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
@@ -4,5 +4,9 @@ export const Container = styled(Box, {
   margin: '$6 auto 0',
   padding: 0,
   display: 'grid',
+  maxWidth: '100%',
   position: 'relative',
+
+  width: 540,
+  gridTemplateColumns: '1fr',
 })
