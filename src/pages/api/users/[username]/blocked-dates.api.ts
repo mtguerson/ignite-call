3cbb671c12c640cb39e26fb083b2fbe424966906
mtguerson <mtguerson@gmail.com>
2024-08-21 10:30:59 -0400

    deploy: database deployed

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -43,23 +43,25 @@ export default async function handle(
   })
 
   const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
-    SELECT 
-      EXTRACT(DAY FROM S.date) AS date,
-      COUNT(S.date) AS amount,
-      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size 
+    SELECT
+      EXTRACT(DAY FROM S.DATE) AS date,
+      COUNT(S.date),
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
 
     FROM schedulings S
 
     LEFT JOIN user_time_intervals UTI
-      on UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
+      ON UTI.week_day = EXTRACT(DOW FROM S.date + INTERVAL '1 day')
 
     WHERE S.user_id = ${user.id}
-      AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+      AND EXTRACT(YEAR FROM S.date) = ${year}::int
+      AND EXTRACT(MONTH FROM S.date) = ${month}::int
 
-    GROUP BY EXTRACT(DAY FROM S.date),
+    GROUP BY EXTRACT(DAY FROM S.DATE),
       ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
 
-    HAVING amount >= size
+    HAVING
+      COUNT(S.date) >= ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60);
   `
 
   const blockedDates = blockedDatesRaw.map((item) => item.date)
